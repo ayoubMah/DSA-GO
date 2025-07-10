26f8b0e1e38231518201612643a6e05d99c0b2f8
ayoubMah <ayoubelmahjouby9@gmail.com>
2025-07-10 17:15:36 +0200

    play wiht arrays is go

--- hello.go
+++ hello.go
@@ -3,5 +3,56 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println("gi")
+	var arr = [] int {1,2,3,4,5}
+	fmt.Println(SumArr(arr))
+
+	fmt.Println(SequentialSearch(arr, 6))
+
+	fmt.Println(BinarySearch(arr, 1))
+}
+
+
+func SumArr(arr[] int) int{
+	var j int
+	sum:= 0
+	for j = 0; j < len(arr); j++{
+		sum += arr[j]
+	}
+	return sum
+
+}
+
+func SequentialSearch(arr[] int, value int) bool {
+
+	var j int
+	for j = 0; j < len(arr) ; j++{
+		if arr[j] == value{
+			break
+		}
+	}
+	if j == len(arr) {
+		return false
+	}
+	return true
+
+}
+
+// TODO => NOT COMPLETED
+func BinarySearch(arr[] int, value int) bool{
+	var j int
+	low := 0
+	high := len(arr)
+	mid := low + (low + high)/2
+
+	for j = 0; j < high; j++ {
+		if arr[mid] == value {
+			return true
+		} else if value > arr[mid]{
+			low = mid
+		}else {
+			high = mid + 1
+		}
+	}
+	return false
+
 }
